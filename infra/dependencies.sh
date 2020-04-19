8a7976ed7db024e4c4835b4d5d8ac63e9c33b980
rarnal <arnal.romain@gmail.com>
2020-04-19 13:21:47 +0200

    feat: add kubernetes infra

---
+++ infra/dependencies.sh
@@ -0,0 +1,13 @@
+# add admin role to my account
+kubectl create clusterrolebinding cluster-admin-binding \
+  --clusterrole=cluster-admin \
+  --user=$(gcloud config get-value core/account)
+
+# set up cert-manager
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.yaml
+
+# set up nginx controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+
+
