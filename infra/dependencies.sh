b00dd98839ea82faa49075792a209b9f8066a934
rarnal <arnal.romain@gmail.com>
2020-06-02 21:58:18 +0200

    infra: migrating kubernetes cluster from gcp to digital ocean

--- infra/dependencies.sh
+++ infra/dependencies.sh
@@ -1,13 +1,14 @@
 # add admin role to my account
-kubectl create clusterrolebinding cluster-admin-binding \
-  --clusterrole=cluster-admin \
-  --user=$(gcloud config get-value core/account)
+# kubectl create clusterrolebinding cluster-admin-binding \
+#   --clusterrole=cluster-admin \
+#   --user=$(gcloud config get-value core/account)
 
 # set up cert-manager
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.yaml
 
 # set up nginx controller
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.32.0/deploy/static/mandatory.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/do/deploy.yaml
 
 kubectl create configmap promtail-config --from-file ./infra/conf/grafana/promtail.yaml
