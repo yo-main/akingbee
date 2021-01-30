555877f9cf6753bdb37c5248a683401d69e4e53b
rarnal <arnal.romain@gmail.com>
2021-01-31 00:33:56 +0100

    feat(poseidon): manage locale in antd config provider

--- poseidon/src/App.js
+++ poseidon/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Router } from '@reach/router';
+import { ConfigProvider } from 'antd';
 import './services/localization';
 
 import { NotFound, PublicFrame, PrivateFrame, LoginPage, RegistrationPage, WelcomePage, SetupPage, ApiaryPage, ApiaryCreationPage, HivePage, HiveCreationPage, HiveDetailsPage } from './pages';
@@ -14,70 +15,72 @@ class App extends React.Component {
 
   render () {
     return (
-      <Router >
-        <PrivateFrame path="/" languageCallback={this.changeLanguage}>
-          <WelcomePage path="/" />
-          <NotFound default />
-        </PrivateFrame>
+      <ConfigProvider locale={window.locale}>
+        <Router >
+          <PrivateFrame path="/" languageCallback={this.changeLanguage}>
+            <WelcomePage path="/" />
+            <NotFound default />
+          </PrivateFrame>
 
-        <PrivateFrame path="/manage" languageCallback={this.changeLanguage} />
-        <PrivateFrame path="/manage/apiary" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_apiary} submenuItem={sections.submenu_apiary_list}>
-          <ApiaryPage path="/"/>
-        </PrivateFrame>
+          <PrivateFrame path="/manage" languageCallback={this.changeLanguage} />
+          <PrivateFrame path="/manage/apiary" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_apiary} submenuItem={sections.submenu_apiary_list}>
+            <ApiaryPage path="/"/>
+          </PrivateFrame>
 
-        <PrivateFrame path="/manage/apiary/create" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_apiary} submenuItem={sections.submenu_apiary_create}>
-          <ApiaryCreationPage path="/" />
-        </PrivateFrame>
+          <PrivateFrame path="/manage/apiary/create" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_apiary} submenuItem={sections.submenu_apiary_create}>
+            <ApiaryCreationPage path="/" />
+          </PrivateFrame>
 
-        <PrivateFrame path="/manage/hive" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_hive} submenuItem={sections.submenu_hive_list}>
-          <HivePage path="/" />
-        </PrivateFrame>
+          <PrivateFrame path="/manage/hive" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_hive} submenuItem={sections.submenu_hive_list}>
+            <HivePage path="/" />
+          </PrivateFrame>
 
-        <PrivateFrame path="/manage/hive/create" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_hive} submenuItem={sections.submenu_hive_create}>
-          <HiveCreationPage path="/" />
-        </PrivateFrame>
+          <PrivateFrame path="/manage/hive/create" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_hive} submenuItem={sections.submenu_hive_create}>
+            <HiveCreationPage path="/" />
+          </PrivateFrame>
 
-        <PrivateFrame path="/manage/hive/:hiveId" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_hive} submenuItem={sections.submenu_hive_list}>
-          <HiveDetailsPage path="/" />
-        </PrivateFrame>
+          <PrivateFrame path="/manage/hive/:hiveId" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_hive} submenuItem={sections.submenu_hive_list}>
+            <HiveDetailsPage path="/" />
+          </PrivateFrame>
 
-        <PrivateFrame path="/setup" languageCallback={this.changeLanguage} />
+          <PrivateFrame path="/setup" languageCallback={this.changeLanguage} />
 
-        <PrivateFrame path="/setup/apiary/honey_type" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_apiary} submenuItem={sections.submenu_setup_apiary_honey_type}>
-          <SetupPage path="/" dataType={setupData.apiary_honey_type}/>
-        </PrivateFrame>
+          <PrivateFrame path="/setup/apiary/honey_type" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_apiary} submenuItem={sections.submenu_setup_apiary_honey_type}>
+            <SetupPage path="/" dataType={setupData.apiary_honey_type}/>
+          </PrivateFrame>
 
-        <PrivateFrame path="/setup/swarm/health" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_swarm} submenuItem={sections.submenu_setup_swarm_health}>
-          <SetupPage path="/" dataType={setupData.swarm_health_status}/>
-        </PrivateFrame>
+          <PrivateFrame path="/setup/swarm/health" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_swarm} submenuItem={sections.submenu_setup_swarm_health}>
+            <SetupPage path="/" dataType={setupData.swarm_health_status}/>
+          </PrivateFrame>
 
-        <PrivateFrame path="/setup/hive/beekeeper" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_hive} submenuItem={sections.submenu_setup_hive_beekeeper}>
-          <SetupPage path="/" dataType={setupData.hive_beekeeper}/>
-        </PrivateFrame>
+          <PrivateFrame path="/setup/hive/beekeeper" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_hive} submenuItem={sections.submenu_setup_hive_beekeeper}>
+            <SetupPage path="/" dataType={setupData.hive_beekeeper}/>
+          </PrivateFrame>
 
-        <PrivateFrame path="/setup/hive/condition" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_hive} submenuItem={sections.submenu_setup_hive_condition}>
-          <SetupPage path="/" dataType={setupData.hive_condition}/>
-        </PrivateFrame>
+          <PrivateFrame path="/setup/hive/condition" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_hive} submenuItem={sections.submenu_setup_hive_condition}>
+            <SetupPage path="/" dataType={setupData.hive_condition}/>
+          </PrivateFrame>
 
-        <PrivateFrame path="/setup/event/type" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_event} submenuItem={sections.submenu_setup_event_type}>
-          <SetupPage path="/" dataType={setupData.event_type}/>
-        </PrivateFrame>
+          <PrivateFrame path="/setup/event/type" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_event} submenuItem={sections.submenu_setup_event_type}>
+            <SetupPage path="/" dataType={setupData.event_type}/>
+          </PrivateFrame>
 
-        <PrivateFrame path="/setup/event/status" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_event} submenuItem={sections.submenu_setup_event_status}>
-          <SetupPage path="/" dataType={setupData.event_status}/>
-        </PrivateFrame>
+          <PrivateFrame path="/setup/event/status" languageCallback={this.changeLanguage} submenuTopic={sections.submenu_setup_event} submenuItem={sections.submenu_setup_event_status}>
+            <SetupPage path="/" dataType={setupData.event_status}/>
+          </PrivateFrame>
 
-        <PrivateFrame path="/profil" languageCallback={this.changeLanguage}/>
+          <PrivateFrame path="/profil" languageCallback={this.changeLanguage}/>
 
-        <PublicFrame path="/login" languageCallback={this.changeLanguage}>
-          <LoginPage path="/" />
-        </PublicFrame>
+          <PublicFrame path="/login" languageCallback={this.changeLanguage}>
+            <LoginPage path="/" />
+          </PublicFrame>
 
-        <PublicFrame path="/register" languageCallback={this.changeLanguage}>
-          <RegistrationPage path="/" />
-        </PublicFrame>
+          <PublicFrame path="/register" languageCallback={this.changeLanguage}>
+            <RegistrationPage path="/" />
+          </PublicFrame>
 
-      </Router>
+        </Router>
+      </ConfigProvider>
     );
   }
 }
--- poseidon/src/services/localization.js
+++ poseidon/src/services/localization.js
@@ -1,10 +1,16 @@
 /* global i18n */
+import 'moment/locale/fr';
+
+import enUS from 'antd/lib/locale/en_US';
+import frFR from 'antd/lib/locale/fr_FR';
+
 import fr from '../locales/french.json';
 import en from '../locales/english.json';
 
 import { setCookie, getCookie } from '../lib/common.js';
 
 const languages = { en, fr };
+const locales = { en: enUS, fr: frFR };
 
 window.i18n = (key) => window.i18nData[key];
 
@@ -12,12 +18,15 @@ window.changeLanguage = (lang) => {
   if (lang in languages) {
     window.i18nData = languages[lang];
     window.currentLanguage = lang;
+    window.locale = locales[lang];
     setCookie('language', lang, {expires: 'Fri, 31 Dec 2100 23:59:59 GMT'});
   }
 }
 
 const getDefaultLanguage = () => {
-  const lang = getCookie('language') || window.navigator.language;
-  return (lang === 'fr' ? 'fr' : 'en');
+  const defaultLang = getCookie('language') || window.navigator.language;
+  let lang = defaultLang === 'fr' ? 'fr' : 'en';
+  return lang;
 }
+
 window.changeLanguage(getDefaultLanguage());
