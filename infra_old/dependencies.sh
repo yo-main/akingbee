3d54cad28e162a0dd94dc96d054c501e952f7c61
rarnal <arnal.romain@gmail.com>
2021-02-27 23:50:57 +0100

    feat: add new infra

--- infra_old/dependencies.sh
+++ infra_old/dependencies.sh
@@ -4,10 +4,11 @@
 #   --user=$(gcloud config get-value core/account)
 
 # set up cert-manager
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.yaml
 
 # set up nginx controller
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.32.0/deploy/static/mandatory.yaml
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/do/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/do/deploy.yaml
 
+kubectl create configmap promtail-config --from-file ./infra/conf/grafana/promtail.yaml
