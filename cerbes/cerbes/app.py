45219c272593de4c56380f3e1c5ebdbd2a431cb4
rarnal <arnal.romain@gmail.com>
2021-03-03 23:13:34 +0100

    feat(cerbes): add akingbee to cors

--- cerbes/cerbes/app.py
+++ cerbes/cerbes/app.py
@@ -8,7 +8,12 @@ def create_app():
     middleware = MiddleWare(db_client=db())
     client = AppClient(routers=router, middleware=middleware)
     client.add_cors_middleware(
-        allow_origins=["http://localhost:3000", "http://localhost:8080"],
+        allow_origins=[
+            "http://localhost:3000",
+            "http://localhost:8080",
+            "http://akingbee.local",
+            "http://akingbee.com",
+        ],
         allow_credentials=True,
     )
     return client.get_app()
