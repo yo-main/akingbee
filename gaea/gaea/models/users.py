3d1bd364b1f9d69c65fa9e6ee78870b2cb56fadd
rarnal <arnal.romain@gmail.com>
2021-03-14 23:01:40 +0100

    feat(gaea): add activation_id to user

--- gaea/gaea/models/users.py
+++ gaea/gaea/models/users.py
@@ -12,6 +12,7 @@ class Users(Base):
     __tablename__ = "users"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = Column(TEXT(), unique=True, nullable=False)
+    activation_id = Column(UUID(as_uuid=True), nullable=True, default=uuid.uuid4)
 
     created_at = Column(TIMESTAMP(), default=datetime.datetime.utcnow)
     updated_at = Column(
