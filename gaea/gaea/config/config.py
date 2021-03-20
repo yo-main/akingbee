70f446b4f126a91d90156529cc7e0199e632dab8
rarnal <arnal.romain@gmail.com>
2021-03-20 23:52:27 +0100

    fix(gaea): add new file for dynaconf

--- gaea/gaea/config/config.py
+++ gaea/gaea/config/config.py
@@ -2,7 +2,7 @@ from dynaconf import Dynaconf
 
 CONFIG = Dynaconf(
     envvar_prefix="AKB",
-    settings_files=["config.yaml"],
+    settings_files=["config.yaml", ".env"],
     env_switcher="ENV",
     core_loaders=["YAML"],
     environments=True,
