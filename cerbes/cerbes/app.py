e7660c20202190093a2c717314bd156312525645
Yomain <arnal.romain@gmail.com>
2023-02-05 16:51:56 +0100

    20 mins more gona in the wild

--- cerbes/cerbes/app.py
+++ cerbes/cerbes/app.py
@@ -1,5 +1,6 @@
 from gaea.database import db
 from gaea.webapp import AppClient, MiddleWare
+from gaea.config import CONFIG
 
 from cerbes.views import router
 
@@ -7,10 +8,13 @@ from cerbes.views import router
 def create_app():
     middleware = MiddleWare(db_client=db())
     client = AppClient(routers=router, middleware=middleware)
-    client.add_cors_middleware(
-        # allow_origin_regex=r"^https?://(.*\.)?((akingbee\.(com|test))|localhost)(:\d+)?$",
-        allow_credentials=True,
-    )
+
+    if not CONFIG.get("DISABLE_CORS", False):
+        client.add_cors_middleware(
+            allow_origin_regex=r"^https?://(.*\.)?((akingbee\.(com|test))|localhost)(:\d+)?$",
+            allow_credentials=True,
+        )
+
     return client.get_app()
 
 
