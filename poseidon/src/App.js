24403d6c3c37780b789185d545c08c10d6af44f2
Yomain <arnal.romain@gmail.com>
2023-05-20 06:53:08 +0200

    feat(poseidon): bump antd version

--- poseidon/src/App.js
+++ poseidon/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 import { BrowserRouter, Switch, withRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import './services/localization';
--- poseidon/src/components/appHeaders.js
+++ poseidon/src/components/appHeaders.js
@@ -6,40 +6,35 @@ import { HomeOutlined } from '@ant-design/icons';
 import * as constants from '../constants';
 import { logOff, getLoggerUserData } from '../services/authentication';
 
-class LanguageMenu extends React.Component {
+class LanguageMenuItem extends React.Component {
   render() {
     const items = Object.values(constants.languages).map((country) => (
       <Menu.Item key={country}>{country}</Menu.Item>
     ));
 
-    return (
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[window.currentLanguage]}>
-        <Menu.SubMenu key="language_menu" title={window.i18n("word.language")} onClick={this.props.callback} id='language-menu'>
+    return <Menu.SubMenu key="language_menu" title={window.i18n("word.language")} onClick={this.props.callback} id='language-menu'>
           {items}
-        </Menu.SubMenu>
-      </Menu>
-    );
+        </Menu.SubMenu>;
   }
 }
 
 export function LoggedOutMenu({ languageCallback }) {
+
   return (
     <Row justify="center">
       <Col span={12}>
         <Menu theme="dark" mode="horizontal" selectedKeys={[""]}>
-          <Menu.Item><Link to={`/`}><HomeOutlined style={{fontSize: '17px'}} /></Link></Menu.Item>
+          <Menu.Item><Link to={`/`}><HomeOutlined style={{ fontSize: '17px' }} /></Link></Menu.Item>
         </Menu>
       </Col>
       <Col span={4} >
-        <span style={{color: '#404040'}}>AKingBee {process.env.REACT_APP_VERSION}</span>
+        <span style={{ color: '#404040' }}>AKingBee {process.env.REACT_APP_VERSION}</span>
       </Col>
       <Col span={8}>
         <Row justify="end">
-          <Col>
-            <LanguageMenu callback={languageCallback} />
-          </Col>
-          <Col>
+          <Col flex={1} push={12}>
             <Menu theme="dark" mode="horizontal" selectedKeys={[]}>
+              <LanguageMenuItem callback={languageCallback} />
               <Menu.Item><Link to={`/login`}>{window.i18n("word.login")}</Link></Menu.Item>
               <Menu.Item><Link to={`/register`}>{window.i18n("word.register")}</Link></Menu.Item>
             </Menu>
@@ -58,7 +53,7 @@ export function LoggedInMenu({ languageCallback, section }) {
   }
 
   let username = getLoggerUserData("username")
-  let is_admin= getLoggerUserData("admin") || getLoggerUserData("impersonator_id");
+  let is_admin = getLoggerUserData("admin") || getLoggerUserData("impersonator_id");
 
   let admin_menu = <></>;
   if (is_admin) {
@@ -68,28 +63,26 @@ export function LoggedInMenu({ languageCallback, section }) {
 
   return (
     <Row justify="space-between">
-      <Col>
+      <Col span={10}>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[section]}>
-          <Menu.Item><Link to={`/`}><HomeOutlined style={{fontSize: '17px'}} /></Link></Menu.Item>
+          <Menu.Item><Link to={`/`}><HomeOutlined style={{ fontSize: '17px' }} /></Link></Menu.Item>
           <Menu.Item key={constants.sections.menu_manage}><Link to={`/manage`}>{window.i18n("word.manage")}</Link></Menu.Item>
           <Menu.Item key={constants.sections.menu_setup}><Link to={`/setup`}>{window.i18n("word.setup")}</Link></Menu.Item>
           <Menu.Item key={constants.sections.menu_profil}><Link to={`/profil`}>{window.i18n("word.profil")}</Link></Menu.Item>
           {admin_menu}
         </Menu>
       </Col>
-      <Col>
-          <span style={{color: '#404040'}}>AKingBee {process.env.REACT_APP_VERSION}</span>
+      <Col span={4}>
+        <span style={{ color: '#404040' }}>AKingBee {process.env.REACT_APP_VERSION}</span>
       </Col>
-      <Col>
-        <Row>
-          <Col>
-            <span style={{color: 'white'}}>{window.i18n("word.welcome")} {username}</span>
+      <Col span={8}>
+        <Row justify="end">
+          <Col flex="none" push={11}>
+            <span style={{ color: 'white' }}>{window.i18n("word.welcome")} {username}</span>
           </Col>
-          <Col>
-            <LanguageMenu callback={languageCallback} />
-          </Col>
-          <Col>
+          <Col flex="auto" push={12}>
             <Menu theme="dark" mode="horizontal">
+              <LanguageMenuItem callback={languageCallback} />
               <Menu.Item onClick={onClick}>{window.i18n("word.logout")}</Menu.Item>
             </Menu>
           </Col>
