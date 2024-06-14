36680f59551dc25ddaa9f804c2548423cbc3765c
Yomain <arnal.romain@gmail.com>
2024-06-14 22:59:16 +0200

    chore: remove src folder, update venv and clean

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShellNoCC {
 
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
   packages = with pkgs; [ 
     gcc
+    just
     rust-analyzer
     rustup
   ];
