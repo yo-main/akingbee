4c0556c7f62b2a0a48f5bdc2cfb621f144ecdcad
Yomain <arnal.romain@gmail.com>
2024-08-25 22:17:41 +0200

    feat: create/get user in sqlite

---
+++ database/repositories/users.go
@@ -0,0 +1,58 @@
+package repositories
+
+import (
+	"akingbee/models"
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
+	_ "github.com/google/uuid"
+)
+
+const createCredentials = `
+	INSERT INTO CREDENTIALS (
+		public_id, username, password
+	) VALUES (
+		$1, $2, $3
+	)
+`
+
+func CreateCredentials(ctx context.Context, db *sql.DB, credentials *models.Credentials) error {
+	_, err := db.ExecContext(ctx, createCredentials, credentials.PublicId, credentials.Username, credentials.Password)
+	return err
+}
+
+const createUser = `
+	INSERT INTO USERS (
+		public_id, email, credential_id
+	) VALUES (
+		$1, $2, (SELECT ID FROM CREDENTIALS WHERE CREDENTIALS.PUBLIC_ID=$3)
+	)
+`
+
+func CreateUser(ctx context.Context, db *sql.DB, user *models.User) error {
+	_, err := db.ExecContext(ctx, createUser, user.PublicId, user.Email, user.Credentials.PublicId)
+	return err
+}
+
+const getUser = `
+	SELECT USERS.public_id, USERS.email, CREDENTIALS.public_id, CREDENTIALS.username, CREDENTIALS.password
+	FROM USERS
+	JOIN CREDENTIALS ON USERS.CREDENTIAL_ID=CREDENTIALS.ID
+	WHERE USERS.public_id=$1
+`
+
+func GetUser(ctx context.Context, db *sql.DB, publicId *uuid.UUID) (*models.User, error) {
+	row := db.QueryRowContext(ctx, getUser, publicId)
+
+	var user models.User
+	var credentials models.Credentials
+
+	err := row.Scan(&user.PublicId, &user.Email, &credentials.PublicId, &credentials.Username, &credentials.Password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module akingbee
+
+go 1.21.11
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.22
+)
