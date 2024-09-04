a8b1cc72c60180b80efe07c9733379941571a473
Yomain <arnal.romain@gmail.com>
2024-09-04 22:35:25 +0200

    Add post user endpoint

--- app/user/api/create_user.go
+++ app/user/api/create_user.go
@@ -1,20 +1,42 @@
 package user
 
 import (
-	"akingbee/api/templates"
-	"akingbee/models"
-	"log"
+	user_service "akingbee/app/user"
+	"akingbee/app/user/models"
+	"encoding/json"
 	"net/http"
 )
 
-func HandlePostUser(response http.ResponseWriter, req *http.Request) {
-	username := req.PostForm.Get("username")
-	password := req.PostForm.Get("password")
-
-
-	user := models.User{
-		PublicId: ,
+func userToJson(user *models.User) ([]byte, error) {
+	data := map[string]interface{}{
+		"publicId": user.PublicId,
+		"email":    user.Email,
+		"username": user.Credentials.Username,
 	}
 
-	
+	return json.Marshal(data)
+}
+
+func HandlePostUser(response http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+
+	username := req.PostForm.Get("username")
+	password := req.PostForm.Get("password")
+	email := req.PostForm.Get("email")
+
+	user, err := user_service.CreateUser(ctx, email, username, password)
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	payload, err := userToJson(user)
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	response.WriteHeader(http.StatusOK)
+	response.Header().Set("Content-Type", "application/json")
+	response.Write(payload)
 }
