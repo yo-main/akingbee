cf036638f076bac67c29df5a34406f3584cbf55f
Yomain <arnal.romain@gmail.com>
2024-09-13 22:08:32 +0200

    Add apiary table

--- bees/pages/apiary.go
+++ bees/pages/apiary.go
@@ -11,14 +11,36 @@ import (
 
 var apiaryPageTemplate = template.Must(pages.HtmlPage.ParseFiles("bees/pages/templates/apiary.html"))
 
+type apiaryPageParameter struct {
+	Table components.Table
+}
+
 func HandleGetApiary(response http.ResponseWriter, req *http.Request) {
 	menu, err := components.GetLoggedInMenu()
 	if err != nil {
 		return
 	}
 
+	params := apiaryPageParameter{
+		Table: components.Table{
+			IsBordered:  false,
+			IsStripped:  true,
+			IsFullWidth: true,
+			Headers: []components.Header{
+				{Label: "Nom"},
+				{Label: "Lieu"},
+				{Label: "Type de miel"},
+				{Label: "Nombre de ruches"},
+			},
+			Rows: []components.Rows{
+				{Values: []string{"Ma rucher", "Paris", "Noix", "10"}},
+				{Values: []string{"Mon autre rucher", "Ganges", "Tournesol", "5"}},
+				{Values: []string{"Mon beau rucher", "Puteaux", "Fleur", "8"}},
+			},
+		}}
+
 	var apiaryPage bytes.Buffer
-	err = pages.HtmlPage.ExecuteTemplate(&apiaryPage, "apiary.html", nil)
+	err = pages.HtmlPage.ExecuteTemplate(&apiaryPage, "apiary.html", params)
 
 	if err != nil {
 		log.Printf("Failed to build login page: %s", err)
