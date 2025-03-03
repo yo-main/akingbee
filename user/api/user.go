e96ca6bc3ff34b9d2b8dc6034c2f13bc01d2b5d5
Yomain <arnal.romain@gmail.com>
2025-03-03 15:17:10 +0100

    Put api for login in a specific file

--- user/api/user.go
+++ user/api/user.go
@@ -5,7 +5,6 @@ import (
 	user_pages "akingbee/user/pages"
 	"akingbee/user/services"
 	"akingbee/web"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -52,47 +51,4 @@ func HandlePostUser(response http.ResponseWriter, req *http.Request) {
 	htmx.PushUrl(response, "/login")
 	response.Write(loginPage.Bytes())
 	response.WriteHeader(http.StatusOK)
-}
-
-func HandlePostLogin(response http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
-
-	username := req.FormValue("username")
-	password := req.FormValue("password")
-
-	if len(username) == 0 || len(password) == 0 {
-		log.Print("No parameter provided")
-		response.WriteHeader(http.StatusBadGateway)
-		return
-	}
-
-	token, err := services.LoginUser(ctx, username, password)
-
-	if err != nil {
-		log.Printf("Login failure: %s", err)
-		web.PrepareFailedNotification(response, err.Error())
-		response.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	welcomePage, err := user_pages.GetWelcomePage(req)
-
-	htmx.PushUrl(response, "/")
-	web.PrepareLoggedInMenu(req, response, username)
-	web.PrepareSuccessNotification(response, fmt.Sprintf("Hello %s !", username))
-	response.Header().Set("Set-Cookie", fmt.Sprintf("%s=%s; HttpOnly; Secure", "akingbeeToken", token))
-	response.Write(welcomePage.Bytes())
-}
-
-func HandleLogout(response http.ResponseWriter, req *http.Request) {
-	response.Header().Set("Set-Cookie", "akingbeeToken=''; expire;")
-	web.PrepareLoggedOutMenu(response)
-	htmx.PushUrl(response, "/")
-
-	welcomePage, err := user_pages.GetWelcomePage(req)
-	if err != nil {
-		log.Printf("Could not load welcome page: %s", err)
-	} else {
-		response.Write(welcomePage.Bytes())
-	}
 }
