5ed9e1217cb29ad6476b63df6a682fecef9817b3
Yomain <arnal.romain@gmail.com>
2025-04-20 16:02:12 +0200

    Add a migrations table to keep track of... migrations

--- internal/database/queries/migrations/migrations.go
+++ internal/database/queries/migrations/migrations.go
@@ -16,64 +16,78 @@ import (
 func Upgrade(ctx context.Context, dbClient *sql.DB) {
 	log.Print("Running migrations")
 
-	err := M0001.Upgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
+	dbClient.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS migrations(version INTEGER NOT NULL);")
 
-	err = M0002.Upgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
+	currentVersion := getCurrentVersion(ctx, dbClient)
 
-	err = M0003.Upgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
-
-	err = M0004.Upgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
-
-	err = M0005.Upgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
-
-	err = M0006.Upgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
+	execUpgrade(M0001.Upgrade, ctx, dbClient, &currentVersion, 1)
+	execUpgrade(M0002.Upgrade, ctx, dbClient, &currentVersion, 2)
+	execUpgrade(M0003.Upgrade, ctx, dbClient, &currentVersion, 3)
+	execUpgrade(M0004.Upgrade, ctx, dbClient, &currentVersion, 4)
+	execUpgrade(M0005.Upgrade, ctx, dbClient, &currentVersion, 5)
+	execUpgrade(M0006.Upgrade, ctx, dbClient, &currentVersion, 6)
 }
 
 func Downgrade(ctx context.Context, dbClient *sql.DB) {
-	err := M0006.Downgrade(ctx, dbClient)
+	currentVersion := getCurrentVersion(ctx, dbClient)
+
+	execDowngrade(M0006.Downgrade, ctx, dbClient, &currentVersion, 5)
+	execDowngrade(M0005.Downgrade, ctx, dbClient, &currentVersion, 4)
+	execDowngrade(M0004.Downgrade, ctx, dbClient, &currentVersion, 3)
+	execDowngrade(M0003.Downgrade, ctx, dbClient, &currentVersion, 2)
+	execDowngrade(M0002.Downgrade, ctx, dbClient, &currentVersion, 1)
+	execDowngrade(M0001.Downgrade, ctx, dbClient, &currentVersion, 0)
+}
+
+func execUpgrade(
+	migration func(ctx context.Context, dbClient *sql.DB) error,
+	ctx context.Context,
+	dbClient *sql.DB,
+	currentVersion *int8,
+	aimedVersion int8,
+) {
+	if *currentVersion+1 == aimedVersion {
+		err := migration(ctx, dbClient)
+		if err != nil {
+			log.Panicf("Migration failed: %s", err)
+		}
+
+		*currentVersion = aimedVersion
+		updateVersion(ctx, dbClient, *currentVersion)
+	}
+}
+
+func execDowngrade(
+	migration func(ctx context.Context, dbClient *sql.DB) error,
+	ctx context.Context,
+	dbClient *sql.DB,
+	currentVersion *int8,
+	aimedVersion int8,
+) {
+	if *currentVersion-1 == aimedVersion {
+		err := migration(ctx, dbClient)
+		if err != nil {
+			log.Panicf("Migration failed: %s", err)
+		}
+
+		*currentVersion = aimedVersion
+		updateVersion(ctx, dbClient, *currentVersion)
+	}
+}
+
+func getCurrentVersion(ctx context.Context, dbClient *sql.DB) int8 {
+	var currentVersion int8
+	row := dbClient.QueryRowContext(ctx, "SELECT version FROM migrations;")
+	err := row.Scan(&currentVersion)
 	if err != nil {
 		log.Panicf("Migration failed: %s", err)
 	}
 
-	err = M0005.Downgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
+	return currentVersion
+}
 
-	err = M0004.Downgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
-
-	err = M0003.Downgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
-
-	err = M0002.Downgrade(ctx, dbClient)
-	if err != nil {
-		log.Panicf("Migration failed: %s", err)
-	}
-
-	err = M0001.Downgrade(ctx, dbClient)
+func updateVersion(ctx context.Context, dbClient *sql.DB, version int8) {
+	_, err := dbClient.ExecContext(ctx, "DELETE FROM migrations; INSERT INTO migrations(version) values($1);", version)
 	if err != nil {
 		log.Panicf("Migration failed: %s", err)
 	}
