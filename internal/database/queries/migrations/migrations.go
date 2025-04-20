6fddb6b9c1efe66e305dab2046ab50c65c5ca1c1
Yomain <arnal.romain@gmail.com>
2025-04-20 11:28:57 +0200

    make comment.hive_id nullable

--- internal/database/queries/migrations/migrations.go
+++ internal/database/queries/migrations/migrations.go
@@ -10,6 +10,7 @@ import (
 	"akingbee/internal/database/queries/migrations/M0003"
 	"akingbee/internal/database/queries/migrations/M0004"
 	"akingbee/internal/database/queries/migrations/M0005"
+	"akingbee/internal/database/queries/migrations/M0006"
 )
 
 func Upgrade(ctx context.Context, dbClient *sql.DB) {
@@ -17,45 +18,63 @@ func Upgrade(ctx context.Context, dbClient *sql.DB) {
 
 	err := M0001.Upgrade(ctx, dbClient)
 	if err != nil {
-		log.Printf("Migration failed: %s", err)
-		return
+		log.Panicf("Migration failed: %s", err)
 	}
 
 	err = M0002.Upgrade(ctx, dbClient)
 	if err != nil {
-		log.Printf("Migration failed: %s", err)
-		return
+		log.Panicf("Migration failed: %s", err)
 	}
 
 	err = M0003.Upgrade(ctx, dbClient)
 	if err != nil {
-		log.Printf("Migration failed: %s", err)
-		return
+		log.Panicf("Migration failed: %s", err)
 	}
 
 	err = M0004.Upgrade(ctx, dbClient)
 	if err != nil {
-		log.Printf("Migration failed: %s", err)
-		return
+		log.Panicf("Migration failed: %s", err)
 	}
 
 	err = M0005.Upgrade(ctx, dbClient)
 	if err != nil {
-		log.Printf("Migration failed: %s", err)
-		return
+		log.Panicf("Migration failed: %s", err)
+	}
+
+	err = M0006.Upgrade(ctx, dbClient)
+	if err != nil {
+		log.Panicf("Migration failed: %s", err)
 	}
 }
 
 func Downgrade(ctx context.Context, dbClient *sql.DB) {
-	err := M0002.Downgrade(ctx, dbClient)
+	err := M0006.Downgrade(ctx, dbClient)
 	if err != nil {
-		log.Printf("Migration failed: %s", err)
-		return
+		log.Panicf("Migration failed: %s", err)
+	}
+
+	err = M0005.Downgrade(ctx, dbClient)
+	if err != nil {
+		log.Panicf("Migration failed: %s", err)
+	}
+
+	err = M0004.Downgrade(ctx, dbClient)
+	if err != nil {
+		log.Panicf("Migration failed: %s", err)
+	}
+
+	err = M0003.Downgrade(ctx, dbClient)
+	if err != nil {
+		log.Panicf("Migration failed: %s", err)
+	}
+
+	err = M0002.Downgrade(ctx, dbClient)
+	if err != nil {
+		log.Panicf("Migration failed: %s", err)
 	}
 
 	err = M0001.Downgrade(ctx, dbClient)
 	if err != nil {
-		log.Printf("Migration failed: %s", err)
-		return
+		log.Panicf("Migration failed: %s", err)
 	}
 }
